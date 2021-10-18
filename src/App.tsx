c7a8fe7a09473666dd3299929887dcf3f7e4c8a3
yugorbit <yugo.nakamura.0101@gmail.com>
2021-10-19 00:05:52 +0900

    色々コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,121 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+
+/* Firestore */
+import { getFirestore, getDocs, collection, CollectionReference } from "firebase/firestore"
+
+type Task = {
+  created: Date,
+  detail: string,
+  driver: string,
+  title: string
+}
+
+async function getTaskList(): Promise<Task[]> {
+  const db = getFirestore();
+  const ref: CollectionReference = collection(db, "tasks");
+  const q = await getDocs(ref);
+
+  const tasks: Task[] = [];
+  q.forEach((task) => {
+    tasks.push({
+      created: task.data().created,
+      detail: task.data().detail,
+      driver: task.data().driver,
+      title: task.data().title
+    });
+  });
+  return tasks;
+}
+
 
 function App() {
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [input, setInput] = useState({
+    title: "",
+    driver: "Yugo"
+  });
+  const [count, setCount] = useState(0);
+  const intervalRef = useRef(null);
+
+  const getTaskListFromFirestore = async () => {
+    const tasks = await getTaskList();
+    setTasks(tasks);
+  }
+
+  const addTaskToFirestore = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    console.log(input);
+  }
+
+  const handleChangeInputTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput({
+      title: e.target.value,
+      driver: input.driver
+    });
+  }
+
+  const handleChangeInputDriver = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setInput({
+      title: input.title,
+      driver: e.target.value
+    });
+  }
+
+  const timerStart = useCallback(() => {
+    if (intervalRef.current !== null) {
+      return;
+    }
+  }, []);
+
+  useEffect(() => {
+    getTaskListFromFirestore();
+  }, []);
+
+  const TaskList = (<ul className="tasklist">
+    {
+      tasks.map((task, i) => {
+        return (
+          <li className="taskitem" key={i}>
+            <p className="taskitem_name">{task.title}</p>
+            <p className="taskitem_driver">{task.driver}</p>
+          </li>)
+      })
+    }</ul >)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="task">
+        <p className="heading">タスク一覧</p>
+        {TaskList}
+        <form className="taskform" name="addTaskForm">
+          <input className="taskform_name" type="text" name="taskName" placeholder="新しいタスク名を入力"
+            value={input.title} onChange={(e) => {
+              handleChangeInputTitle(e)
+            }}></input>
+          <div className="taskform_asignee" >
+            <select value={input.driver} onChange={(e) => {
+              handleChangeInputDriver(e)
+            }}>
+              <option value="Kei">Kei</option>
+              <option value="Yugo">Yugo</option>
+            </select>
+          </div>
+          <button className="taskform_button" onClick={(e) => { addTaskToFirestore(e) }}>追加</button>
+        </form>
+      </div>
+      <div className="timer">
+        <div className="timer_circle" id="">
+          <svg width="240" height="240" viewBox="0, 0, 200, 200" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="100" cy="100" r="90" strokeWidth="12" stroke="#006989" fill="none" />
+          </svg>
+          <div className="timer_circle_count">25:00</div>
+        </div>
+        <button className="timer_start">タイマースタート</button>
+      </div>
+    </div >
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { initializeApp } from "firebase/app";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +17,19 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBffkomiozV_0pGm4uaoUBI6AFxQBA1dY",
+  authDomain: "nemuke-e4852.firebaseapp.com",
+  projectId: "nemuke-e4852",
+  storageBucket: "nemuke-e4852.appspot.com",
+  messagingSenderId: "36070398332",
+  appId: "1:36070398332:web:dee332a8f86dd54ed56cf9",
+  measurementId: "G-M0KFJ0BJTB"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
